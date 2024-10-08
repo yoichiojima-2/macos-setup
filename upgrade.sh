2f7c009527b34d70bbb8e9b45545e311b90a280d
Yoichi Ojima <yoichiojima@gmail.com>
2024-10-08 23:07:34 +0900

    refactor: upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -3,12 +3,16 @@
 upgrade() {
     brew upgrade
     pip list | tail -n +3 | awk '{ print $1 }' | xargs pip install --upgrade
-    upgrade_python_venv
+    upgrade_python_libs
+}
+
+upgrade_python_libs(){
+    pip list | tail -n +3 | awk '{ print $1 }' | xargs pip install --upgrade
 }
 
 upgrade_python_venv() {
     . ~/python-venv/general-purpose/bin/activate
-    pip list | tail -n +3 | awk '{ print $1 }' | xargs pip install --upgrade
+    upgrade_python_libs
 }
 
 upgrade
